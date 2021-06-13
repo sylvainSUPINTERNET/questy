760159bead31812aaceee2959657571c8ccb02b7
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-06-13 23:45:39 +0200

    [FIX] port host

--- conf/config.py
+++ conf/config.py
@@ -1,4 +1,5 @@
 import os
-
-WS_HOST=str(os.environ.get('HEROKU_APP_NAME', 'localhost'))
-WS_PORT=int(os.environ.get('PORT', 8765))
+WS_HOST="0.0.0.0"
+WS_PORT=5000
+#WS_HOST=str(os.environ.get('HEROKU_APP_NAME', 'localhost'))
+#WS_PORT=int(os.environ.get('PORT', 8765))
