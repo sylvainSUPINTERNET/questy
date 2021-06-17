c4d4304c8063386559bb93e23317c747f63d429f
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-06-17 23:46:03 +0200

    [WIP pubsub]

--- app.py
+++ app.py
@@ -16,22 +16,12 @@ scheduler.start()
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
+r = redis.Redis(host='localhost', port=6379, db=0)
+p = r.pubsub()
+p.psubscribe('hello*')
+r.publish('hello2', 'New Test Message 2')
 
-cache = redis.Redis(host='localhost', port=6379)
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
-count = get_hit_count()
-print(count)
+print(p.get_message())
 
 # Check every minutes if some quests must be deleted in redis DB
 logging.info("Init schedule for removing quests")
