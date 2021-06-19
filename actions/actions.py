a27468b140bbdd6af1a50e68d4c040ca4d5ace06
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-06-20 00:12:28 +0200

    [UPDATE] create quest to redis stream

--- actions/actions.py
+++ actions/actions.py
@@ -2,34 +2,29 @@ from tasks.Quest import Quest
 from conf.redisConf import QUESTS_STREAM_NAME
 import logging
 
+
+
 def create_quest(data, redis_instance):
     new_quest = Quest(data["title"], data["description"], data["rewards"], data["level_gain"], data["requirements"])
-
     # Check if stream exists or not
-    if redis_instance.xlen(QUESTS_STREAM_NAME) != 0 :
-        print("Quests stream already exists")
-    else:
-        logging.info("Init quests stream ...")
-        keys_values = new_quest.__dict__
-        for k,v in keys_values.items():
+    keys_values = new_quest.__dict__
+    for k,v in keys_values.items():
 
             # Cleanup tuple format from WS payload (since Redis don't accept this type)
-            if type(v) is tuple:
-                tmp = ""
-                for m in keys_values[k]:
-                    for idx,z in enumerate(m) :
-                        if idx == len(m) - 1:
-                            tmp += str(z);
-                        else:
-                            tmp += str(z) + ",";
-                keys_values[k] = tmp
+        if type(v) is tuple:
+            tmp = ""
+            for m in keys_values[k]:
+                for idx,z in enumerate(m) :
+                    if idx == len(m) - 1:
+                        tmp += str(z);
+                    else:
+                        tmp += str(z) + ",";
+            keys_values[k] = tmp
 
-                
-        print(keys_values)
-        # redis_instance.xadd(QUESTS_STREAM_NAME, keys_values, id='*', maxlen=None, approximate=True)
+    redis_instance.xadd(QUESTS_STREAM_NAME, keys_values, id='*', maxlen=None, approximate=True)                
         
-        # logging.info(f"Quest added with success {str(keys_values)}")
-        # logging.info("Stream created with success.")
+    logging.info(f"Quest added with success {str(keys_values)}")
+    logging.info("Stream created with success.")
     
 
 # p = r.xadd("quests", fields, id='*', maxlen=None, approximate=True)
